23622cbbd967fa9e85e9d11193c29489b1e73e67
oliver-perez <35386069+oliver-perez@users.noreply.github.com>
2026-04-27 05:38:32 -0600

    Initial Commit

---
+++ AsyncLeakDemo/AsyncLeakDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLeakDemoApp.swift
+//  AsyncLeakDemo
+//
+//  Created by Oliver Perez on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AsyncLeakDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncLeakDemoTests/AsyncLeakDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLeakDemoTests.swift
+//  AsyncLeakDemoTests
+//
+//  Created by Oliver Perez on 27/04/26.
+//
+
+import Testing
+@testable import AsyncLeakDemo
+
+struct AsyncLeakDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
